9eabe69aaf96808ef6cccc3d1538ca5efc534960
evgen <evgen@funtoo>
2013-03-12 16:47:34 +0600

    modified:   invaders.py 	modified:   main.py 	deleted:    moving.py

--- invaders.py
+++ invaders.py
@@ -4,38 +4,23 @@ import pygame
 class Invaders():
     '''class for working with monster's'''
 
-    def __init__(self, posX, posY, name, di):
+    def __init__(self, posX, posY, name):
         '''
         init monster
         '''
         self.x = posX
         self.y = posY
-        self.m_speed_init = 10
-        self.m_speed = self.m_speed_init
         if name == 'boo':
-            self.m = glob.glob('boo*png')
+            self.m = glob.glob('boo1.png')
         elif name == 'kva':
-            self.m = glob.glob('kva*png')
+            self.m = glob.glob('kva1.png')
         self.m.sort()
-        self.m_pos = 0
-        self.m_max = len(self.m)-1
         self.img = pygame.image.load(self.m[0])
         self.img = pygame.transform.scale(self.img, (50,50))
-        self.update(di,0)
 
-    def update(self, di, pos):
+    def update(self, di):
         '''
         update/animated moster
         '''
-        if pos != 0:
-            self.m_speed -= 1
-            self.x += pos
-            if self.m_speed == 0:
-                self.img = pygame.image.load(self.m[self.m_pos])
-                self.img = pygame.transform.scale(self.img, (50,50))
-                self.m_speed = self.m_speed_init
-                if self.m_pos == self.m_max:
-                    self.m_pos = 0
-                else:
-                    self.m_pos += 1
+
         di.blit(self.img,(self.x,self.y))
--- main.py
+++ main.py
@@ -13,34 +13,30 @@ clock = pygame.time.Clock()
 enemies = []
 
 for x in range(5):
-    enemies.append(invaders.Invaders(50*x+50, 50, 'boo', screen))
-    enemies.append(invaders.Invaders(50*x+50, 100, 'kva', screen))
-    enemies.append(invaders.Invaders(50*x+50, 150, 'boo', screen))
+    enemies.append(invaders.Invaders(50*x+50, 50, 'boo'))
+    enemies.append(invaders.Invaders(50*x+50, 100, 'kva'))
 
 print (enemies)
-pos = 0
-enemyspeed = 0.5
+enemyspeed = 15
+
 while 1:
+
+    ev = pygame.event.poll()
+    if ev.type == pygame.QUIT:
+        sys.exit()
 
     screen.fill((0,0,0))
     clock.tick(60)
 
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            sys.exit()
-
     for i in range(len(enemies)):
         enemies[i].x += enemyspeed
-        if pos == 0:
-            enemies[i].update(screen,pos)
-            pos = 1
-        if pos == 1:
-            enemies[i].update(screen,pos)
-            pos = 0
+        enemies[i].update(screen)
 
-        if enemies[len(enemies)-1].x > 500:
-            enemyspeed = -2
-        if enemies[0].x < 50:
-            enemyspeed = 0.5
+    if enemies[len(enemies)-1].x > 500:
+        enemyspeed = -15
 
-    pygame.display.update()
+    if enemies[0].x < 100:
+        enemyspeed = 15
+
+    pygame.display.flip()
+
