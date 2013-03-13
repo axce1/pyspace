078e5262db433f376dc299e8d602227f905ed8f1
evgen <evgen@funtoo>
2013-03-13 13:07:14 +0600

    add hero

--- main.py
+++ main.py
@@ -17,22 +17,23 @@ for x in range(5):
     enemies.append(invaders.Invaders(50*x+50, 100, 'kva'))
 
 
-hero = pygame.image.load('spacy.png')
-hero = pygame.transform.scale(hero, (50,30))
+hero = invaders.Player(200,450)
 
 playtime = 0
 enemyspeed = 2
-x = 200
+
 while 1:
 
     for ev in pygame.event.get():
+
         if ev.type == pygame.QUIT:
             sys.exit()
+
         if ev.type == pygame.KEYDOWN:
-            if ev.key == pygame.K_RIGHT:
-                x += 25
-            if ev.key == pygame.K_LEFT:
-                x -= 25
+            if ev.key == pygame.K_RIGHT and hero.px < 750:
+                hero.px += 5
+            if ev.key == pygame.K_LEFT  and hero.px > 50:
+                hero.px -= 5
 
 
     screen.fill((0,0,0))
@@ -49,6 +50,7 @@ while 1:
     if enemies[0].x < 50:
         enemyspeed = 2
 
-    screen.blit(hero, (x,450))
+    hero.update(screen)
+
     pygame.display.set_caption('Space Invaders - %.2f fps - %.2f' %(clock.get_fps(), playtime))
     pygame.display.flip()
